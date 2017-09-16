b5d570911808784fe2a70053448a5c3d482c3ad7
Liam Taylor <liam.taylor888@googlemail.com>
2017-09-16 17:54:29 +0100

    refactor to factory and module

--- index.js
+++ index.js
@@ -7,66 +7,77 @@ TARGET_FOLDER = "./"
 
 METADATA_FIELDS = ["title", "contributors"]
 
-function readFolder(folderPath){
-  fs.readdir(folderPath, function(err, list){
+const EPubReader = function(sourceFolder, targetFolder){
+  this.sourceFolder = sourceFolder || SOURCE_FOLDER;
+  this.targetFolder = targetFolder || TARGET_FOLDER
+  this.jsonFields = ["title", "contributors"];
+};
+
+EPubReader.prototype.readFolder = function(){
+  const self = this;
+  fs.readdir(this.sourceFolder, function(err, list){
     const books = [];
     for(var i=0;i<list.length;i++){
       const book = list[i]
       const bookPath = SOURCE_FOLDER + book;
       const fileName = book.split(".").shift()
-      makeDirectory(book)
-      const metaData = getMetaData(bookPath,fileName)
-    }
-  })
+      self.makeDirectory(book)
+      const metaData = self.getMetaData(bookPath,fileName)
+    };
+  });
 };
 
-function getMetaData(path, fileName){
+EPubReader.prototype.getMetaData = function(path, fileName){
+  const self = this;
   const epub = new ePub(path)
   epub.on("end", () => {
     const metaData = epub.metadata
-    const json = createJson(metaData, METADATA_FIELDS)
-    addJsonToDir(json, fileName)
+    const json = self.createJson(metaData)
+    self.addJsonToDir(json, fileName)
   });
   epub.parse();
-}
+};
 
-function makeDirectory(name){
-  const dir = TARGET_FOLDER + name.split(".").shift()
+EPubReader.prototype.makeDirectory = function(name){
+  const self = this;
+  const dir = self.targetFolder + name.split(".").shift()
   if(!fs.existsSync(dir)){
     fs.mkdir(dir, () => {
       console.log("Created directory at '" + dir + "'" )
     });
-  }
+  };
 };
 
-function createJson(data, fields){
+EPubReader.prototype.createJson = function(data){
+  const self = this;
   obj = {}
-  for(var i=0;i<fields.length;i++){
-    const field = fields[i]
+  for(var i=0;i<self.jsonFields.length;i++){
+    const field = self.jsonFields[i]
     if(data[field]){
       obj[field] = data[field]
     } else {
       obj[field] = "Not present";
-    }
-  }
+    };
+  };
   return JSON.stringify(obj)
-}
+};
 
-function addJsonToDir(json, title){
+
+EPubReader.prototype.addJsonToDir = function(json, title){
   const fileName = title + ".json"
   const dir = TARGET_FOLDER
   const fileAtPath = dir + title + "/"+ fileName
   if(fs.existsSync(fileAtPath)){
-    console.log("File '" + fileName +"' already exists at '"+ dir +"'")
+    console.log("File '" + fileName +"' already exists at '"+ fileAtPath +"'")
   } else {
     const jsonFile = fs.writeFile(fileAtPath, json, (err) =>{
       if(err){
         console.log(err)
       } else {
         console.log("JSON file created at" + fileAtPath)
-      }
-    })
-  }
-}
+      };
+    });
+  };
+};
 
-readFolder(SOURCE_FOLDER)
+exports.EPubReader = EPubReader;
